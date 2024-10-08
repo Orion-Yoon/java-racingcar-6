771593c61a4df9f1a032c7d72a69d4cb790cb7a2
Orion-Yoon <insertlock8@gmail.com>
2024-10-08 20:24:18 +0900

    입출력 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,14 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<String> car = new ArrayList<>();
+        Racing racing = new Racing();
+        View view = new View();
+
+        view.start();
     }
 }
